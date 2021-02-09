a0e54ec3e4af4f2817938c786d7193a5482b93a8
OlderNed <alexiver96@hotmail.com>
2021-02-09 13:13:05 +0100

    finished tutorial, and added movement to player

--- Robo Rally/src/main/java/inf112/skeleton/app/HelloWorld.java
+++ Robo Rally/src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -2,18 +2,25 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 
-public class HelloWorld implements ApplicationListener {
+public class HelloWorld extends InputAdapter implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
     private TiledMap map;
@@ -29,18 +36,18 @@ public class HelloWorld implements ApplicationListener {
     private TiledMapTileLayer rightWallLayer;
     private TiledMapTileLayer leftWallLayer;
 
-    private TiledMapTileLayer yellowConveyerRight;
-    private TiledMapTileLayer yellowConveyerLeft;
-    private TiledMapTileLayer yellowConveyerUp;
-    private TiledMapTileLayer yellowConveyerDown;
+    private TiledMapTileLayer yellowConveyorRight;
+    private TiledMapTileLayer yellowConveyorLeft;
+    private TiledMapTileLayer yellowConveyorUp;
+    private TiledMapTileLayer yellowConveyorDown;
 
     // Clockwise rotating yellow conveyors
-    private TiledMapTileLayer yellowConveyerClockWiseRight;
-    private TiledMapTileLayer yellowConveyerClockWiseLeft;
-    private TiledMapTileLayer yellowConveyerClockWiseUp;
-    private TiledMapTileLayer yellowConveyerClockWiseDown;
+    private TiledMapTileLayer yellowConveyorClockWiseRight;
+    private TiledMapTileLayer yellowConveyorClockWiseLeft;
+    private TiledMapTileLayer yellowConveyorClockWiseUp;
+    private TiledMapTileLayer yellowConveyorClockWiseDown;
 
-    // Counte clockwise rotating yellow convyors
+    // Counter clockwise rotating yellow conveyors
     private TiledMapTileLayer yellowConveyorCClockwiseUpLeft;
     private TiledMapTileLayer yellowConveyorCClockwiseLeftDown;
     private TiledMapTileLayer yellowConveyorCClockwiseDownRight;
@@ -49,6 +56,12 @@ public class HelloWorld implements ApplicationListener {
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
     private TmxMapLoader loader;
+
+    private Vector2 playerPosition;
+    private TiledMapTileLayer.Cell playerCell;
+    private TiledMapTileLayer.Cell playerWonCell;
+    private TiledMapTileLayer.Cell playerDiedCell;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -60,22 +73,20 @@ public class HelloWorld implements ApplicationListener {
         // yymap = loader.load("/Users/alexanderiversen/Documents/inf122/tutorial/Tutorial/src/main/java/inf112/skeleton/assets/example.tmx");
         map = loader.load("Vault.tmx");
 
-
-
         boardLayer = (TiledMapTileLayer) map.getLayers().get("Board");
         playerLayer = (TiledMapTileLayer) map.getLayers().get("Player");
         flagLayer = (TiledMapTileLayer) map.getLayers().get("Flag");
         holeLayer = (TiledMapTileLayer) map.getLayers().get("Hole");
 
-        yellowConveyerRight = (TiledMapTileLayer) map.getLayers().get("ConveyorRight");
-        yellowConveyerLeft = (TiledMapTileLayer) map.getLayers().get("ConveyorLeft");
-        yellowConveyerUp = (TiledMapTileLayer) map.getLayers().get("ConveyorUp");
-        yellowConveyerDown = (TiledMapTileLayer) map.getLayers().get("ConveyorDown");
+        yellowConveyorRight = (TiledMapTileLayer) map.getLayers().get("ConveyorRight");
+        yellowConveyorLeft = (TiledMapTileLayer) map.getLayers().get("ConveyorLeft");
+        yellowConveyorUp = (TiledMapTileLayer) map.getLayers().get("ConveyorUp");
+        yellowConveyorDown = (TiledMapTileLayer) map.getLayers().get("ConveyorDown");
 
-        yellowConveyerClockWiseRight = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateUpRight");
-        yellowConveyerClockWiseLeft = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateDownLeft");
-        yellowConveyerClockWiseUp = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateLeftUp");
-        yellowConveyerClockWiseDown = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateRightDown");
+        yellowConveyorClockWiseRight = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateUpRight");
+        yellowConveyorClockWiseLeft = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateDownLeft");
+        yellowConveyorClockWiseUp = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateLeftUp");
+        yellowConveyorClockWiseDown = (TiledMapTileLayer) map.getLayers().get("ConveyorRotateRightDown");
 
         // CClockwise rotations
 
@@ -84,12 +95,31 @@ public class HelloWorld implements ApplicationListener {
         yellowConveyorCClockwiseDownRight= (TiledMapTileLayer) map.getLayers().get("ConveyorRotateDownRight");
         yellowConveyorCClockwiseRightUp= (TiledMapTileLayer) map.getLayers().get("ConveyorRotateRightUp");
 
+        Texture texture = new Texture("player.png");
+        TextureRegion[][] portraits = TextureRegion.split(texture, 300, 300);
+
+        playerPosition = new Vector2(1, 1);
+        playerCell = new TiledMapTileLayer.Cell();
+        playerWonCell = new TiledMapTileLayer.Cell();
+        playerDiedCell = new TiledMapTileLayer.Cell();
+
+
+        playerCell.setTile(new StaticTiledMapTile(portraits[0][0]));
+        playerDiedCell.setTile(new StaticTiledMapTile(portraits[0][1]));
+        playerWonCell.setTile(new StaticTiledMapTile(portraits[0][2]));
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 12, 12);
+        playerLayer.setCell(1, 1, playerCell);
+        camera.update();
+
         float size = (float) 1.0/300.0f;
         renderer = new OrthogonalTiledMapRenderer(map, size);
         renderer.setView(camera);
+
+        Gdx.input.setInputProcessor(this);
+
+
 
 
     }
@@ -105,8 +135,17 @@ public class HelloWorld implements ApplicationListener {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-        renderer.render();
 
+        if (holeLayer.getCell((int)playerPosition.x,(int)playerPosition.y) != null) {
+            playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerDiedCell);
+        }
+        if(flagLayer.getCell((int)playerPosition.x, (int)playerPosition.y) != null){
+            playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerWonCell);
+
+        }
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
 
         // batch.begin();
         //font.draw(batch, "Hello World", 200, 200);
@@ -124,4 +163,62 @@ public class HelloWorld implements ApplicationListener {
     @Override
     public void resume() {
     }
+
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        Vector2 character_copy = playerPosition;
+        if(i == Input.Keys.LEFT)
+            playerPosition = new Vector2(playerPosition.x-1,playerPosition.y);
+        playerLayer.setCell((int)character_copy.x,(int)character_copy.y,null);
+        playerLayer.setCell((int)playerPosition.x,(int)playerPosition.y,playerCell);
+        if(i == Input.Keys.RIGHT)
+            playerPosition = new Vector2(playerPosition.x+1,playerPosition.y);
+        playerLayer.setCell((int)character_copy.x,(int)character_copy.y,null);
+        playerLayer.setCell((int)playerPosition.x,(int)playerPosition.y,playerCell);
+        if(i == Input.Keys.UP)
+            playerPosition = new Vector2(playerPosition.x,playerPosition.y+1);
+        playerLayer.setCell((int)character_copy.x,(int)character_copy.y,null);
+        playerLayer.setCell((int)playerPosition.x,(int)playerPosition.y,playerCell);
+        if(i == Input.Keys.DOWN)
+            playerPosition = new Vector2(playerPosition.x,playerPosition.y-1);
+        playerLayer.setCell((int)character_copy.x,(int)character_copy.y,null);
+        playerLayer.setCell((int)playerPosition.x,(int)playerPosition.y,playerCell);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int i) {
+        return false;
+    }
+
 }
