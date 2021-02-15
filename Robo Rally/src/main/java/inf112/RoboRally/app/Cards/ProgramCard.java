06bd9c96f9b24e28edcfdefb1d938163eb2abf3d
mikkelangel0 <55317463+mikkelangel0@users.noreply.github.com>
2021-02-15 15:30:23 +0100

    Update ProgramCard.java

--- Robo Rally/src/main/java/inf112/RoboRally/app/Cards/ProgramCard.java
+++ Robo Rally/src/main/java/inf112/RoboRally/app/Cards/ProgramCard.java
@@ -1,9 +1,9 @@
 package inf112.RoboRally.app.Cards;
 
 public class ProgramCard {
-    private int move;
-    private String turn;
-    private int priority;
+    private int move; // 0 hvis snukort, 1-3 ellers
+    private String turn; // høyre, venstre, opp, ned, nei for move kort feks?
+    private int priority; // prioriteten på hvilke kort som skal brukes først
     private String filename;
 
 }
