42493eb05d7500b1279eea0611572e23989d5d71
gudolv <gudolv@hotmail.com>
2021-02-15 15:39:22 +0100

    forgot to add IPlayer interface to git

---
+++ Robo Rally/src/main/java/inf112/RoboRally/app/Objects/IPlayer.java
@@ -0,0 +1,16 @@
+package inf112.RoboRally.app.Objects;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Vector2;
+
+public interface IPlayer {
+    TiledMapTileLayer.Cell getState();
+
+    int getHp();
+
+    boolean visitFlag(Flag flag);
+
+    Vector2 getPosition();
+
+    void setDamage(int x);
+}
