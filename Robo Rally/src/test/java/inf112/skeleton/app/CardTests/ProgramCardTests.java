48d3f00722257711d25e398cd0e5aa66481c6f54
Arild Grimstveit <arild@grimstveit.no>
2021-03-23 14:53:11 +0100

    adds 84 images with their respective priority number

--- Robo Rally/src/test/java/inf112/skeleton/app/CardTests/ProgramCardTests.java
+++ Robo Rally/src/test/java/inf112/skeleton/app/CardTests/ProgramCardTests.java
@@ -1,9 +1,10 @@
 package inf112.skeleton.app.CardTests;
 
-import static org.junit.Assert.*;
+import inf112.RoboRally.app.Cards.ProgramCard;
 import org.junit.Before;
 import org.junit.Test;
-import inf112.RoboRally.app.Cards.ProgramCard;
+
+import static org.junit.Assert.assertEquals;
 
 public class ProgramCardTests {
     ProgramCard moveOneCard;
@@ -16,13 +17,13 @@ public class ProgramCardTests {
 
     @Before
     public void setUp(){
-        moveOneCard = new ProgramCard(1, new String(), 650, "Move-1.jpg");
-        moveTwoCard = new ProgramCard(2, new String(), 780, "Move-2.jpg");
-        moveThreeCard = new ProgramCard(3, new String(), 840, "Move-3.jpg");
-        uTurnCard = new ProgramCard(0, "U-Turn", 60, "U-Turn.jpg");
-        rotateRightCard = new ProgramCard(0, "RotateRight", 420, "RotateRight.jpg");
-        rotateLeftCard = new ProgramCard(0, "RotateLeft", 410, "RotateLeft.jpg");
-        backUpCard = new ProgramCard(0, "Back-Up", 480, "Back-Up.jpg");
+        moveOneCard = new ProgramCard(1, new String(), 650, "Move-1-490.jpg");
+        moveTwoCard = new ProgramCard(2, new String(), 780, "Move-2-670.jpg");
+        moveThreeCard = new ProgramCard(3, new String(), 840, "Move-3-790.jpg");
+        uTurnCard = new ProgramCard(0, "U-Turn", 60, "U-Turn-10.jpg");
+        rotateRightCard = new ProgramCard(0, "RotateRight", 420, "RotateRight-70.jpg");
+        rotateLeftCard = new ProgramCard(0, "RotateLeft", 410, "RotateLeft-80.jpg");
+        backUpCard = new ProgramCard(0, "Back-Up", 480, "Back-Up-430.jpg");
      }
 
      @Test
@@ -51,6 +52,6 @@ public class ProgramCardTests {
 
      @Test
      public void getFilenameTest(){
-         assertEquals("Move-1.jpg", moveOneCard.getFilename());
+         assertEquals("Move-1-490.jpg", moveOneCard.getFilename());
      }
 }
