4cc16762fc25b4e860de0d76384051e24cc84586
mikkelangel0 <55317463+mikkelangel0@users.noreply.github.com>
2021-03-25 18:58:51 +0100

    Updated GridTests
    Added GridTests::fillGridTest

--- Robo Rally/src/test/java/inf112/skeleton/app/GridTests/GridTests.java
+++ Robo Rally/src/test/java/inf112/skeleton/app/GridTests/GridTests.java
@@ -13,6 +13,11 @@ public class GridTests {
     @Before
     public void setUp() throws Exception {
         grid = new Grid();
-        board = new Board("Vault.tmx");
+    }
+
+    public void fillGridTest(){
+        assertNotNull(grid.grid[2][8]);
+        assertNotNull(grid.grid[9][11]);
+        assertNull(grid.grid[8][10]);
     }
 }
